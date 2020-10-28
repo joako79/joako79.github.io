3fea462e931f3cdf9810f77e29800f4e89773e84
joako79 <64276675+joako79@users.noreply.github.com>
2020-10-28 12:57:20 -0300

    anp

--- data/cerco_3.js
+++
@@ -1,8 +0,0 @@
-var json_cerco_3 = {
-"type": "FeatureCollection",
-"name": "cerco_3",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "ID": 0.0, "IDENT": "Alambrado Perimetral", "LENGHT": "0" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -64.90605008, -42.8112006 ], [ -64.90586249, -42.81199747 ] ] ] } }
-]
-}
